6055f9d7b1a591d880d5b5a3532357a30340d1d0
Bob <tpitorin@myges.fr>
2025-06-19 14:44:47 +0200

    feat: setup spatie role

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,10 +9,14 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Traits\HasRoles;
+
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, HasRoles;
+    
+    protected $with = ['roles', 'permissions'];
 
     protected $table = 'users';
 
