d499c88e5499a1169a9868c0647d2d0b346d72ba
Mr PHP <tpitorin@myges.fr>
2025-07-08 23:12:33 +0200

    feat: cs fixer

---
+++ .php-cs-fixer.php
@@ -0,0 +1,18 @@
+<?php
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@PSR12' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'ordered_imports' => ['sort_algorithm' => 'alpha'],
+        'no_unused_imports' => true,
+    ])
+    ->setFinder(
+        PhpCsFixer\Finder::create()
+            ->in([
+                __DIR__ . '/app',
+                __DIR__ . '/config',
+                __DIR__ . '/database',
+                __DIR__ . '/routes',
+            ])
+    ); 
